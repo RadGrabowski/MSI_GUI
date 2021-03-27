922c65590f16e61f7edc086b1515ea4aebb5aef7
RadGrabowski <radek18g@gmail.com>
2021-03-27 23:12:18 +0100

    Add possibility to integrate and differentiate the data, add buttons to manage that functionality.

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from scipy import signal
 import numpy as np
 import tkinter as tk
 from tkinter import messagebox, ttk
+from scipy.integrate import cumtrapz
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 from navigator import Navigator
 from copy import deepcopy
 
@@ -47,9 +49,11 @@ def hover_leave(event):
 def start_filtering(event):
     """Gets called when the filter type, frequency or order changes. Recalculates then the output based on the newly
     set parameters and plots a new graph on the GUI. """
-    global filtered
+    global filtered, prev_diff
     ax[0, 0].clear()  # clears the subplots before drawing the new ones
     ax[1, 0].clear()
+    diff_check.set('off')
+    differentiate('_')
 
     if filter_type_cbb.get() == 'Off':  # if there is no filter applied, there is no possibility to insert a value in
         # the entries determining filter's parameters
@@ -64,7 +68,7 @@ def start_filtering(event):
             left_cutoff_entry.configure(state='normal')
             filter_order.configure(state='normal')
             if left_cutoff_entry.get() == '':
-                left_cutoff_entry.insert(tk.END, round(fs/5, 2))  # default filtering position if no value was set
+                left_cutoff_entry.insert(tk.END, round(fs / 5, 2))  # default filtering position if no value was set
             if filter_order.get() == '':
                 filter_order.insert(tk.END, 10)  # default filter order if no value was set
             left = round(float(left_cutoff_entry.get()), 2)
@@ -76,7 +80,7 @@ def start_filtering(event):
                 sos = signal.butter(int(filter_order.get()), left, filter_type_cbb.get().lower(), fs=fs,
                                     output='sos')
                 if filter_type_cbb.get() == 'Highpass':
-                    ax[1, 0].axvspan(left, fs/2, alpha=0.3, color='red')
+                    ax[1, 0].axvspan(left, fs / 2, alpha=0.3, color='red')
                 else:
                     ax[1, 0].axvspan(0, left, alpha=0.3, color='red')
 
@@ -85,7 +89,7 @@ def start_filtering(event):
                 label.configure(text='Critical frequencies (Hz):')
                 right_cutoff_entry.configure(state='normal')
                 if right_cutoff_entry.get() == '':
-                    right_cutoff_entry.insert(tk.END, round(fs/3, 2))
+                    right_cutoff_entry.insert(tk.END, round(fs / 3, 2))
                 right = round(float(right_cutoff_entry.get()), 2)
                 if right < left:
                     right = left + fs / 10
@@ -99,7 +103,7 @@ def start_filtering(event):
 
                 if filter_type_cbb.get() == 'Bandstop':
                     ax[1, 0].axvspan(0, left, alpha=0.3, color='red')
-                    ax[1, 0].axvspan(right, fs/2, alpha=0.3, color='red')
+                    ax[1, 0].axvspan(right, fs / 2, alpha=0.3, color='red')
                 else:
                     ax[1, 0].axvspan(left, right, alpha=0.3, color='red')
 
@@ -128,6 +132,40 @@ def start_filtering(event):
     if toolbar._active is None:
         toolbar.forward()
     canvas.draw()
+
+
+def differentiate(callback=None):
+    global filtered, prev_diff
+    ax[0, 1].clear()
+    i = cumtrapz(filtered)
+    d = np.diff(filtered)
+    if diff_check.get() == 'off':
+        if prev_diff == 'off' and callback is None:
+            return
+        ax[0, 1].plot(t[:len(filtered)], filtered, color='#1f77b4', lw=1)
+        prev_diff = 'off'
+        if callback is not None:  # prevents the plot to blink (the cause is the last line of this function)
+            return
+    elif diff_check.get() == 'differentiate':
+        if prev_diff == 'differentiate':
+            return
+        ax[0, 1].plot(t[:len(d)], d, color='#1f77b4', lw=1)
+        prev_diff = 'differentiate'
+    elif diff_check.get() == 'integrate':
+        if prev_diff == 'integrate':
+            return
+        ax[0, 1].plot(t[:len(i)], i, color='#1f77b4', lw=1)
+        prev_diff = 'integrate'
+    ax[0, 1].ticklabel_format(axis='y', style='sci', scilimits=(-2, 2))
+    canvas.draw()
+
+
+def detrend():
+    pass
+
+
+def detrend_line():
+    pass
 
 
 # GETTING THE INPUT SIGNAL, EXTRACTING ITS USEFUL DATA
@@ -163,8 +201,11 @@ can_leave = canvas.mpl_connect('axes_leave_event', hover_leave)
 
 # CREATING OF TOOLBAR AND THE CONTROL PANEL WITH ITS CORRESPONDING BUTTONS AND ENTRIES AND PLACING THEM ON THE MAIN
 # WINDOW
+s = ttk.Style()
+s.configure('White.TCheckbutton', background='white')
+s.configure('White.TRadiobutton', background='white')
 panel = tk.LabelFrame(root, bg='white')
-panel.place(relx=0.75, rely=0.1, anchor="nw", relwidth=0.23, height=150)
+panel.place(relx=0.75, rely=0.1, anchor="nw", relwidth=0.23, height=230)
 filter_type_cbb = ttk.Combobox(panel, values=['Off', 'Lowpass', 'Highpass', 'Bandpass', 'Bandstop'], state='readonly')
 filter_type_cbb.current(0)
 filter_type_cbb.config(width=9)
@@ -184,10 +225,28 @@ right_cutoff_entry.bind("<Return>", lambda event: start_filtering(event))
 right_cutoff_entry.place(x=65, y=70, anchor="w", width=45, height=20)
 right_cutoff_entry.configure(state='disabled')
 
+ttk.Label(panel, text='Filter order', background='white').place(relx=0.05, y=95, anchor="w", width=135, height=20)
+
 filter_order = ttk.Entry(panel, width=6, justify='center')
 filter_order.bind("<Return>", lambda event: start_filtering(event))
 filter_order.place(relx=0.05, y=120, anchor="w", width=45, height=20)
 filter_order.configure(state='disabled')
+
+diff_check = tk.StringVar(value='off')
+prev_diff = 'off'
+trend = tk.BooleanVar(value=False)
+trend_line = tk.BooleanVar(value=False)
+
+ttk.Radiobutton(panel, text='Off', var=diff_check, value='off', style='White.TRadiobutton',
+                command=lambda: differentiate()).place(relx=0.05, y=140)
+ttk.Radiobutton(panel, text='Differentiate', var=diff_check, value='differentiate', style='White.TRadiobutton',
+                command=lambda: differentiate()).place(relx=0.05, y=160)
+ttk.Radiobutton(panel, text='Integrate', var=diff_check, value='integrate', style='White.TRadiobutton',
+                command=lambda: differentiate()).place(relx=0.05, y=180)
+ttk.Checkbutton(panel, text='Detrend', style='White.TCheckbutton', var=trend,
+                command=lambda: detrend()).place(relx=0.05, y=200)
+ttk.Checkbutton(panel, text='Detrending line', style='White.TCheckbutton', var=trend_line,
+                command=lambda: detrend_line()).place(relx=0.4, y=200)
 
 toolbar = Navigator(canvas, root, filter_type_cbb,
                     [left_cutoff_entry, right_cutoff_entry, filter_order, filter_type_cbb],
